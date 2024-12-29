0fc2138fee4de013e9924e0a50d785a3314447ac
Dimitrysaf <demetresmeliates@gmail.com>
2024-12-29 22:43:44 +0200

    First Commit

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'wordpressuser' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '77130392' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
@@ -100,3 +100,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+define('WP_ALLOW_REPAIR', true);
